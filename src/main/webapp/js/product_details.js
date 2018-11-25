a355c3b32892bd1947989a79e78a0ad618e622db
lywechange <276368974@qq.com>
2018-11-25 11:30:48 +0800

    11:30

--- src/main/webapp/js/product_details.js
+++ src/main/webapp/js/product_details.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   getProductSwiper();// 获取宝贝轮播图
   getProductDetail();//获取产品详情参数
+  addCart();// 加入购物车
 }
 // 获取宝贝轮播图
 function getProductSwiper(){
@@ -55,3 +56,40 @@ function getProductDetail(){
     }
   })
 }
+
+/**
+ * Web Storage本地存储
+ * 1.两个接口：分别是localStorage和sessonStorage
+ * 2.四个函数：分别是setItem、getItem、removeItem和clear
+ *   A. localStorage.setItem      存储数据信息到本地localStorage.setItem(键名，键值)
+ *   B. localStorage.getItem      读取本地存储的信息localStorage.getItem(键名)
+ *   C. localStorage.removeItem   删除本地存储的信息localStorage.removeItem(键名)
+ *   D. localStorage.clear        清空所以存储的信息localStorage.clear()
+ * 3.封装后的
+ *   A. localStorage.setItem      存储数据信息到本地myStorage.setItem(键名，键值)
+ *   B. localStorage.getItem      读取本地存储的信息myStorage.getItem(键名)
+ *   C. localStorage.removeItem   删除本地存储的信息myStorage.removeItem(键名)
+ *   D. localStorage.clear        清空所以存储的信息myStorage.clear()
+ */
+// 加入购物车
+function addCart(){
+
+}
+
+myStorage.setItem("coffeeType", "mocha");
+myStorage.setItem("coffeePrice", "28");
+    verify();   //验证本地存储
+    myStorage.removeItem("coffeeType");
+    verify();   //验证coffeeType是否存在
+    myStorage.clear();
+    verify();   //验证coffeeType和coffeePrice是否存在
+ 
+    //自定义验证函数，验证coffeeType和coffeePrice的数据是否存在
+    function verify(){
+        var type = myStorage.getItem("coffeeType");
+        var price =myStorage.getItem("coffeePrice");
+        type = type ? type : '不存在';
+        price = price ? price : '不存在';
+        alert( "coffeeType: " + type + "\n\n" + "coffeePrice: " + price );
+    }
+
--- src/main/webapp/js/public.js
+++ src/main/webapp/js/public.js
@@ -11,4 +11,120 @@ $(function(){
         }
     }
 })
+/**
+ * 浏览器或客户端不兼容localStorage时自动切换到Cookies存储。
+ * 写好一个操作cookie的类和函数，将四个函数的名字和参数还有功能和localStorage保持一致。
+ */
 
+ //《JavaScript权威指南》一书中有实现基于cookie的存储API，我把代码敲下来
+// 另外，书中的代码有错，以下为无BUG版并修改成1000天相当长的存储时间
+window.cookieStorage = (new (function(){
+    var maxage = 60*60*24*1000;
+    var path = '/';
+  
+    var cookie = getCookie();
+  
+    function getCookie(){
+        var cookie = {};
+        var all = document.cookie;
+        if(all === "")
+            return cookie;
+        var list = all.split("; ");
+        for(var i=0; i < list.length; i++){
+            var cookies = list[i];
+            var p = cookies.indexOf("=");
+            var name = cookies.substring(0,p);
+            var value = cookies.substring(p+1);
+            value = decodeURIComponent(value);
+            cookie[name] = value;
+        }
+        return cookie;
+    }
+  
+    var keys = [];
+    for(var key in cookie)
+        keys.push(key);
+  
+    this.length = keys.length;
+  
+    this.key = function(n){
+        if(n<0 || n >= keys.length)
+            return null;
+        return keys[n];
+    };
+  
+    this.setItem = function(key, value){
+        if(! (key in cookie)){
+            keys.push(key);
+            this.length++;
+        }
+  
+        cookie[key] = value;
+        var cookies = key + "=" +encodeURIComponent(value);
+        if(maxage)
+            cookies += "; max-age=" + maxage;
+        if(path)
+            cookies += "; path=" + path;
+  
+        document.cookie = cookies;
+    };
+  
+    this.getItem = function(name){
+        return cookie[name] || null;
+    };
+  
+    this.removeItem = function(key){
+        if(!(key in cookie))
+            return;
+  
+        delete cookie[key];
+  
+        for(var i=0; i<keys.length; i++){
+            if(keys[i] === key){
+                keys.splice(i, 1);
+                break;
+            }
+        }
+        this.length--;
+  
+        document.cookie = key + "=; max-age=0";
+    };
+  
+    this.clear = function(){
+        for(var i=0; i<keys.length; i++)
+            document.cookie = keys[i] + "; max-age=0";
+        cookie = {};
+        keys = [];
+        this.length = 0;
+    };
+  })());
+  
+//本地存储，localStorage类没有存储空间的限制，而cookieStorage有存储大小限制
+//在不支持localStorage的情况下会自动切换为cookieStorage
+window.myStorage = (new (function(){
+ 
+    var storage;    //声明一个变量，用于确定使用哪个本地存储函数
+ 
+    if(window.localStorage){
+        storage = localStorage;     //当localStorage存在，使用H5方式
+    }
+    else{
+        storage = cookieStorage;    //当localStorage不存在，使用兼容方式
+    }
+ 
+    this.setItem = function(key, value){
+        storage.setItem(key, value);
+    };
+ 
+    this.getItem = function(name){
+        return storage.getItem(name);
+    };
+ 
+    this.removeItem = function(key){
+        storage.removeItem(key);
+    };
+ 
+    this.clear = function(){
+        storage.clear();
+    };
+})());
